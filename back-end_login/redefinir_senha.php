1710ddf3d755b43a1e012f76af61f109500bf1de
Felipe <feliphi10@gmail.com>
2025-12-06 15:19:19 -0300

    correção na tela de redefinição de senha

--- back-end_login/redefinir_senha.php
+++ back-end_login/redefinir_senha.php
@@ -60,6 +60,7 @@ if (isset($_GET['token'])) {
 <html lang="pt-br">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Redefinir senha</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
